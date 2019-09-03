68fb288065b2bf605de3bfd4cf6feb79b0bf7b06
Celeste Yost <17mcyost@gmail.com>
2019-09-02 20:06:23 -0400

    test commit lol

---
+++ src/server.js
@@ -0,0 +1,5 @@
+//import
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+const textHandler = require('./textResponses.js');
+const jsonHandler = require('./jsonResponses.js');
